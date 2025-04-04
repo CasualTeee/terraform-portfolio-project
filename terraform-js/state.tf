229e43e367c87e413ba6ec7d62f282fb4001a918
CasualTeee <thudson205@icloud.com>
2025-04-03 23:18:46 -0400

    Finalizing code

--- terraform-js/state.tf
+++ terraform-js/state.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "tt-my-terraform-state"
+    bucket = "th-my-tf-website-state"
     key = "global/s3/terraform.tfstate"
     region = "us-east-2" 
-    dynamodb_table = "terraform-lock-file"
-
-
+    dynamodb_table = "my-db-website-table"
   }
 }
